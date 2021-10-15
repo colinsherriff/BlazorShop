ab5b16a3c7bdd0f27299f70c8893cd95987d6384
Colin Sherriff <colinsherriff@yahoo.com>
2021-10-16 12:03:16 +1300

    Add project files.

---
+++ BlazorShop/Client/Services/ProductService/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using BlazorShop.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorShop.Client.Services.ProductService
+{
+    interface IProductService
+    {
+        List<Product> Products { get; set; }
+        void LoadProducts();
+    }
+}
